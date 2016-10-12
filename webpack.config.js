779636baa3dd1d30115412695f779ed2a694add1
Anthony Ng <newyork.anthonyng@gmail.com>
2016-10-12 08:20:35 -0400

    build(lint, webpack): Set up webpack and linter

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'./src/index.jsx'
+	],
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?/,
+				exclude: /node_modules/,
+				loader: 'babel'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	}
+};
