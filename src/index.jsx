96860c6be750383d6b8a632bbc16d910388e3ee8
Anthony Ng <newyork.anthonyng@gmail.com>
2016-10-13 08:40:38 -0400

    chore(eslint): Test eslint

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import BoardSquare from './components/BoardSquare';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { text: '0, 0' };
+	}
+
 	render() {
-		const test = (x, y) => console.log(x + ':' + y);
+		const test = (x, y) => {
+			this.setState({ text: `${x}, ${y}` });
+		};
 
 		return (
 			<div>
+				<h1>{this.state.text}</h1>
 				<BoardSquare x={0} y={0} alive={true} handleClick={test} />
 				<BoardSquare x={1} y={0} alive={false} handleClick={test} />
 				<BoardSquare x={2} y={0} alive={true} handleClick={test} />
